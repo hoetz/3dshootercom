8b36db3ada55cad2262fe82299de2fd60d041b4a
Florian Hötzinger <florian.hoetzinger@gab-net.com>
2018-05-31 10:44:33 +0200

    update lets encrypt controller to validate requests

--- src/Web/Controllers/LetsEncryptController.cs
+++ src/Web/Controllers/LetsEncryptController.cs
@@ -1,12 +1,36 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 public class LetsEncryptController : Controller
+{
+    public LetsEncryptController(IHostingEnvironment env)
     {
-        [HttpGet]
-        [Route(".well-known/acme-challenge/5Q_q4R2FUTPnQIoQ45hbfbvl3qz4BCqw02V0oeRAOiw")]
-        public string Verify()
-        {
-            Response.ContentType="text/plain";
-            return "5Q_q4R2FUTPnQIoQ45hbfbvl3qz4BCqw02V0oeRAOiw.bJFo0_9avJjgL6RNeBxyurjDMHI8HdakeP7Eho5wo7E";
-        }
+        this.Env = env;
     }
+
+    public IHostingEnvironment Env { get; }
+
+    [HttpGet]
+    [Route(".well-known/acme-challenge/{id}")]
+    public string Verify(string id)
+    {
+        Response.ContentType = "text/plain";
+        var content = string.Empty;
+        var path = Env.WebRootPath;
+        var fullPath = Path.Combine(path, @".well-known\acme-challenge");
+        var dirInfo = new DirectoryInfo(fullPath);
+        var files = dirInfo.GetFiles();
+        foreach (var fileInfo in files)
+        {
+            if (fileInfo.Name == id)
+            {
+                using (var file = System.IO.File.OpenText(fileInfo.FullName))
+                {
+                    return (file.ReadToEnd());
+                }
+            }
+        }
+        return (content);
+    }
+}
